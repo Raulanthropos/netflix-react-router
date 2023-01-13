0daa8e4d1b91077425515d8431105f09136174f7
Raulanthropos <ipsichias@yandex.com>
2023-01-13 15:45:36 +0200

    update

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,26 +1,31 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-    
-// const [movie, setMovie] = useState(undefined)
-// const params = useParams()
-//   console.log('PARAMS', params)
-//   console.log('The movie plot is', params.Plot)
+  const [movieData, setMovieData] = useState({});
+  const { movieID } = useParams();
 
-//  useEffect(() => {
-//     const fetchTheMovie = async () => {
-//     const response = await fetch(`http://www.omdbapi.com/?apikey=be0e715&i=tt0241527`)
-//     const data = await response.json();
-//     let detailsToShow = data.find((m) => m.Plot.toString() === params.Plot)
-//     setMovie(detailsToShow);
-// }}, [])
+  const fetchData = async (movieId) => {
+    try {
+      let response = await fetch(
+        `http://www.omdbapi.com/?apikey=be0e715&i=tt0241527`
+      );
 
-    return (
-        <>
-        <h1>Movie details!</h1>
-        </>
-    )
-}
+      if (response.ok) {
+        let data = await response.json();
+        setMovieData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(movieID);
+  }, [movieID]);
+
+  return<div><h4 className="text-white">{movieData.Title}</h4><img src={movieData.Poster} alt={movieData.Title}/><p className="text-white">{movieData.Plot}</p></div>;
+};
 
 export default MovieDetails;
